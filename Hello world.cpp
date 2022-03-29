d85681ce0319b13a674155d9ca1fa5c4a065db14
1-lobov-1 <klop.555@bk.ru>
2022-03-29 20:20:29 +0300

    Code style changed

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
-int main(){
- std::string name; //Our string var
- std::cin >> name; //Input name
- std::cout << "Hello world from" << name << std::endl; //Output
+int
+main()
+{
+  std::string name;                                     // Our string var
+  std::cin >> name;                                     // Input name
+  std::cout << "Hello world from" << name << std::endl; // Output
 }
