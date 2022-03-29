eb0c241d9b38493480720cb28e911cf9b199bcaf
Данила Лобов <46494556+1-lobov-1@users.noreply.github.com>
2022-03-29 14:24:39 +0000

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(){
- std::string name; //Our string var
- std::cin >> name; //Input name
+ std::string name; //Our string переменная
+ std::cin >> name; //Input имя
  std::cout << "Hello world from" << name << std::endl; //Output
 }
